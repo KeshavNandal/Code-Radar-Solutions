c8dbc3e9c2dec2f17fd638dc99002a1a9d4cee32
Keshav Kumar <103065773+KeshavNandal@users.noreply.github.com>
2025-02-02 21:20:26 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,8 +3,8 @@ int main()
 {
     int x,y;
     char ch;
-    scanf("%d%d",&x,&y);
-    scanf("%c", &ch);
+    scanf("%d %d",&x,&y);
+    scanf(" %c", &ch);
     switch(ch)
     {
         case '-' :
