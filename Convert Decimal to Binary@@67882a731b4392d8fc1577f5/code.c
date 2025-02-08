9a7ef63301d1b2839e8d3dc3a0329aa44f7c3028
Keshav Kumar <103065773+KeshavNandal@users.noreply.github.com>
2025-02-09 04:47:03 +0530

    Solution to Convert Decimal to Binary

---
+++ Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d",&x);
+    while(x>=1)
+    {
+        if (x%2==0)
+        {   
+            y=x/2;
+            x=y;
+            printf("0");
+
+        }
+        else if(x%2!=0)
+        {
+            y=x/2;
+            x=y;
+            printf("1");
+        }
+    }
+}
