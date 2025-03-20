272a50d7dde5853d2cd8cf81fb47f9449da7d637
Keshav Kumar <103065773+KeshavNandal@users.noreply.github.com>
2025-03-20 18:52:41 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,13 @@
+void fibonaciiSeries(int n)
+{
+     int  y = 0, z = 1, a;
+
+    for (int i = 0; i < x; i++)
+    {
+        printf("%d ", y);
+        a = y + z;
+        y = z;
+        z = a;
+    }
+    
+}
